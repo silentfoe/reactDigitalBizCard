73df6cf80c8b6014cf5adaaacce6d761f098f5f6
Spencer Pingitore <silentfoe6@gmail.com>
2023-02-24 00:28:44 -0500

    adding comment for changes that is not shown. I believe I need to rebundle and build again for change to be seen. Will need to do this later

--- Components/Interests.js
+++ Components/Interests.js
@@ -7,7 +7,7 @@ function Interests() {
     return (
         
         <section>
-        
+            
             <h3 className="text-heading">Interests</h3>
             <p className="text-content interest-text">Follower of Jesus. Cat herder. BA in Music. Average chess player. Sleeping expert. Camping aficionado. Lego Master Builder. Breakfast cook. Bike path explorer. Great Lakes lover.</p>
         
@@ -19,3 +19,5 @@ function Interests() {
 
 
 export default Interests
+
+// does not show "cat herder". Need to rebundle and build again for change to be seen I think. Not sure about this. 
